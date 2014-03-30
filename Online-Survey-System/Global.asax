9c2c0e3d631108309be91c4a3fc1cf105b2c123c
KodeKracker <pratapakshay0@gmail.com>
2014-03-30 14:41:37 +0530

    added new files

---
+++ Online-Survey-System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Online_Survey_System.MvcApplication" Language="C#" %>
---
+++ Online-Survey-System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Survey_System.Startup))]
+namespace Online_Survey_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
